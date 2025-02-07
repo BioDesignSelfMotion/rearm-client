dbc15038eb8b08b4e73b0400fab9cd744ca40008
Ryan Jiang <36180026+Ryan-Yuanqing-Jiang@users.noreply.github.com>
2025-02-07 14:23:03 +1100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -104,22 +104,6 @@ export default function RentalDashboard() {
                   rareFind
                   image="https://images.unsplash.com/photo-1568605114967-8130f3a36994?auto=format&fit=crop&w=400"
                 />
-                <RentalCard
-                  title="Designer NY style loft"
-                  category="Entire loft in central business district"
-                  liked
-                  image="https://images.unsplash.com/photo-1600596542815-ffad4c1539a9?auto=format&fit=crop&w=400"
-                />
-                <RentalCard
-                  title="5 minute walk from University of Melbourne"
-                  category="Entire rental unit in Carlton"
-                  image="https://images.unsplash.com/photo-1537726235470-8504e3beef77?auto=format&fit=crop&w=400"
-                />
-                <RentalCard
-                  title="Magnificent apartment next to public transport"
-                  category="Entire apartment rental in Collingwood"
-                  image="https://images.unsplash.com/photo-1618221195710-dd6b41faaea6?auto=format&fit=crop&w=400"
-                />
                 <Divider />
                 <Pagination />
               </Stack>
